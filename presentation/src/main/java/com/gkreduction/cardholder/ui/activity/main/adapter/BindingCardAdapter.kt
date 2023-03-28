52d3d19497b2939192ad6737c5a7c3b3679a3f31
admin <konstantinGridasov@gmail.com>
2023-03-28 21:57:38 +0300

    [update] add CarouselRecyclerview

--- presentation/src/main/java/com/gkreduction/cardholder/ui/activity/main/adapter/BindingCardAdapter.kt
+++ presentation/src/main/java/com/gkreduction/cardholder/ui/activity/main/adapter/BindingCardAdapter.kt
@@ -3,6 +3,7 @@ package com.gkreduction.cardholder.ui.activity.main.adapter
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.gkreduction.domain.entity.Card
+import com.jackandphantom.carouselrecyclerview.CarouselRecyclerview
 
 object BindingCardAdapter {
     @JvmStatic
@@ -11,13 +12,18 @@ object BindingCardAdapter {
         requireAll = false
     )
     fun setListChatsAdapter(
-        view: RecyclerView,
+        view: CarouselRecyclerview,
         items: List<Card>?,
     ) {
         items?.let {
             val adapter = AdapterCard()
             adapter.updateItems(it)
             view.adapter = adapter
+            view.apply {
+                set3DItem(false)
+                setAlpha(true)
+                setInfinite(true)
+            }
         }
 
     }
